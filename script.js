376a419bdeb101a19458c74755d2044ea86a2d23
Natalia Voznesenskaia <natascha199105@gmail.com>
2026-02-02 19:50:16 +0100

    Update script.js and index.htm and add style.css

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+/* fetch("https://natascha-voznesenskaia.github.io/my-json-data/data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+
+    document.body.innerHTML = `<h1>${data.message}</h1>`;
+
+    data.items.forEach((n) => {
+      const p = document.createElement("p");
+      p.textContent = n;
+      document.body.appendChild(p);
+    });
+  })
+  .catch((err) => console.error("Ошибка fetch:", err)); */
+
 fetch("https://natascha-voznesenskaia.github.io/my-json-data/data.json")
   .then((res) => res.json())
-  .then((data) => console.log(data))
+  .then((data) => {
+    const app = document.getElementById("app");
+
+    app.innerHTML = `
+      <h1>${data.message}</h1>
+      <ul>
+        ${data.items.map((n) => `<li>${n}</li>`).join("")}
+      </ul>
+    `;
+  })
   .catch((err) => console.error("Ошибка fetch:", err));
